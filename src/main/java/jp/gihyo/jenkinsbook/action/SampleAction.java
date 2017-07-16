b415c3eff67bb95ba9e69175486e9502d23eba04
inotakuya <i_no_u_u@yahoo.co.jp>
2017-07-16 23:03:31 +0900

    チェックスタイル更新

--- src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
+++ src/main/java/jp/gihyo/jenkinsbook/action/SampleAction.java
@@ -11,79 +11,75 @@ import jp.gihyo.jenkinsbook.dto.SampleDTO;
  * DTO class for SampleServlet.
  */
 public class SampleAction {
-	/**
-	 * First name of the user.
-	 */
-	private String firstName;
-	/**
-	 * Last name of the user.
-	 */
-	private String lastName;
+    /**
+     * First name of the user.
+     */
+    private String firstName;
+    /**
+     * Last name of the user.
+     */
+    private String lastName;
 
-	/**
-	 * Constructor of SampleAction.
-	 */
-	public SampleAction() {
-		this(null, null);
-	}
+    /**
+     * Constructor of SampleAction.
+     */
+    public SampleAction() {
+        this(null, null);
+    }
 
-	/**
-	 * Constructor of SampleAction.
-	 * 
-	 * @param fstName
-	 *            first name of the user
-	 * @param lstName
-	 *            last name of the user
-	 */
-	public SampleAction(final String fstName, final String lstName) {
-		this.firstName = fstName;
-		this.lastName = lstName;
-	}
+    /**
+     * Constructor of SampleAction.
+     * 
+     * @param fstName first name of the user
+     * @param lstName last name of the user
+     */
+    public SampleAction(final String fstName, final String lstName) {
+        this.firstName = fstName;
+        this.lastName = lstName;
+    }
 
-	/**
-	 * Check parameter of http servlet request.
-	 * 
-	 * @param request
-	 *            HttpServletRequest
-	 * @return check result
-	 */
-	public final boolean checkParameter(final HttpServletRequest request) {
+    /**
+     * Check parameter of http servlet request.
+     * 
+     * @param request HttpServletRequest
+     * @return check result
+     */
+    public final boolean checkParameter(final HttpServletRequest request) {
 
-		firstName = request.getParameter("FirstName");
-		if ((firstName == null) || ("".equals(firstName))) {
-			return false;
-		}
+        firstName = request.getParameter("FirstName");
+        if ((firstName == null) || ("".equals(firstName))) {
+            return false;
+        }
 
-		lastName = request.getParameter("LastName");
-		if ((lastName == null) || ("".equals(lastName))) {
-			return false;
-		}
+        lastName = request.getParameter("LastName");
+        if ((lastName == null) || ("".equals(lastName))) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Execute action.
-	 * 
-	 * @param request
-	 *            HttpServletRequest
-	 * @return result jsp file path
-	 */
-	public final String execute(final HttpServletRequest request) {
-		SampleDTO dto = new SampleDTO(firstName, lastName);
+    /**
+     * Execute action.
+     * 
+     * @param request HttpServletRequest
+     * @return result jsp file path
+     */
+    public final String execute(final HttpServletRequest request) {
+        SampleDTO dto = new SampleDTO(firstName, lastName);
 
-		// Select the greeting message accordeing to the time
-		Calendar calendar = Calendar.getInstance();
-		int hour = calendar.get(Calendar.HOUR_OF_DAY);
-		if (hour < 12) {
-			dto.setMessage("Good morning");
-		} else {
-			dto.setMessage("Good afternoon");
-		}
+        // Select the greeting message accordeing to the time
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        if (hour < 12) {
+            dto.setMessage("Good morning");
+        } else {
+            dto.setMessage("Good afternoon");
+        }
 
-		HttpSession session = request.getSession(true);
-		session.setAttribute("dto", dto);
+        HttpSession session = request.getSession(true);
+        session.setAttribute("dto", dto);
 
-		return "./WEB-INF/result.jsp";
-	}
+        return "./WEB-INF/result.jsp";
+    }
 }

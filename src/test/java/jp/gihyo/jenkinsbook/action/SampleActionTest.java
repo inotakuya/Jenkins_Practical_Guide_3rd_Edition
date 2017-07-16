82a9bf0d849a02b6914af0b61579996d5a3a510b
inotakuya <i_no_u_u@yahoo.co.jp>
2017-07-17 01:05:07 +0900

    ああああ

--- src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/action/SampleActionTest.java
@@ -93,6 +93,6 @@ public class SampleActionTest {
     
     @Test
     public void testError(){
-        System.out.println("aaaadffgsdfgsdf");
+        System.out.println("aaaadffgsdfgsdfasd");
     }
 }

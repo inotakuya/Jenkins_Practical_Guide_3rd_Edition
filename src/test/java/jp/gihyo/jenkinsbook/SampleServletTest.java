d4da8d20c72460e59281287e095f2f8332895e48
inotakuya <i_no_u_u@yahoo.co.jp>
2017-07-17 17:49:09 +0900

    テストケース追加

--- src/test/java/jp/gihyo/jenkinsbook/SampleServletTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/SampleServletTest.java
@@ -1,54 +1,67 @@
 package jp.gihyo.jenkinsbook;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.io.IOException;
 import java.lang.reflect.Method;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.powermock.api.mockito.PowerMockito;
 
 import jp.gihyo.jenkinsbook.action.SampleAction;
 
 public class SampleServletTest {
 
+    @Rule
+    public MockitoRule mockito = MockitoJUnit.rule();
+
     @Mock
     HttpServletRequest request;
-    
+
     @Mock
     HttpServletResponse response;
-    
+
     @Mock
     RequestDispatcher dispatcher;
-    
+
+    @Mock
+    HttpSession sesstion;
+
     @InjectMocks
-    SampleServlet sampleServlet = new SampleServlet();
+    SampleServlet sampleServlet = PowerMockito.spy(new SampleServlet());
 
     @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
+    public void setUp() throws ServletException, IOException {
+        doNothing().when(dispatcher).forward(request, response);
+        doNothing().when(sesstion).setAttribute(anyString(), any());
+        when(request.getRequestDispatcher("./WEB-INF/error.jsp")).thenReturn(dispatcher);
+        when(request.getRequestDispatcher("./WEB-INF/result.jsp")).thenReturn(dispatcher);
+        when(request.getSession(true)).thenReturn(sesstion);
+
     }
 
     @Test
     public void testDoGet() throws Exception {
-
-
-        doNothing().when(dispatcher).forward(request, response);
-        when(request.getRequestDispatcher("./WEB-INF/error.jsp")).thenReturn(dispatcher);
         sampleServlet.doGet(request, response);
 
         verify(request).setCharacterEncoding("Shift_JIS");
@@ -57,11 +70,19 @@ public class SampleServletTest {
 
     @Test
     public void doPostTest() throws Exception {
-        doNothing().when(dispatcher).forward(request, response);
-        when(request.getRequestDispatcher("./WEB-INF/error.jsp")).thenReturn(dispatcher);
+        
+        when(request.getParameter("action")).thenReturn(null);
+        sampleServlet.doPost(request, response);
+        
+        when(request.getParameter("action")).thenReturn("hel");
+        sampleServlet.doPost(request, response);
+
         when(request.getParameter("action")).thenReturn("hello");
         sampleServlet.doPost(request, response);
-        verify(request, times(1)).setCharacterEncoding("Windows-31J");
+        
+        when(request.getParameter("FirstName")).thenReturn("a");
+        when(request.getParameter("LastName")).thenReturn("b");
+        sampleServlet.doPost(request, response);
     }
 
     @Test

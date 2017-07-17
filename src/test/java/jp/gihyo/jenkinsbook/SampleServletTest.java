7087fbb7ead18d063e89dd0a8856b2f717f28e44
inotakuya <i_no_u_u@yahoo.co.jp>
2017-07-17 13:35:09 +0900

    テスト追加

--- src/test/java/jp/gihyo/jenkinsbook/SampleServletTest.java
+++ src/test/java/jp/gihyo/jenkinsbook/SampleServletTest.java
@@ -3,8 +3,18 @@ package jp.gihyo.jenkinsbook;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.lang.reflect.Method;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -18,6 +28,21 @@ public class SampleServletTest {
     @Before
     public void setUp() {
         sampleServlet = new SampleServlet();
+    }
+    
+    @Test
+    public void testDoGet() throws Exception {
+        HttpServletRequest request = mock(HttpServletRequest.class);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        
+        doNothing().when(dispatcher).forward(request, response);
+        when(request.getRequestDispatcher("./WEB-INF/error.jsp")).thenReturn(dispatcher);
+        new SampleServlet().doGet(request, response);
+        
+        verify(request).setCharacterEncoding("Shift_JIS");
+        verify(request,times(1)).getRequestDispatcher(anyString());
     }
 
     @Test
